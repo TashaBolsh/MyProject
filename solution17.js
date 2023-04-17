41d25df696bf4aa482ff56ea18c03a139d2e5cbb
Tasha <129745242+TashaBolsh@users.noreply.github.com>
2023-04-17 08:39:02 +0300

    Конструкция меню
    /* 
        Задание 5:
        
        Условную конструкцию из задания 4, перепишите с помощью Switch Case
    */

---
+++ solution17.js
@@ -0,0 +1,19 @@
+/* 
+    Задание 5:
+    
+    Условную конструкцию из задания 4, перепишите с помощью Switch Case
+*/
+
+const randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber)
+switch (true){
+    case (randomNumber < 20):
+        console.log("randomNumber меньше 20");
+        break;
+    case (randomNumber > 50):
+        console.log("randomNumber больше 50");
+        break;
+    default:
+        console.log("randomNumber больше 20, и меньше 50");
+}
+
